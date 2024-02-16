19f63fcc4a5741908361493ba86f8c08a10e05d9
Samir <sobiroff23@gmail.com>
2024-02-16 18:16:58 +0500

    fixed

---
+++ script.js
@@ -0,0 +1,9 @@
+var username = prompt("Введите имя пользователя:");
+var password = prompt("Введите пароль:");
+
+
+if (username === "user" && password === "pass") {
+    console.log("Login Gained");
+} else {
+    console.log("Login Failed");
+}
